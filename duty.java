057f6ab538fab80d62955f91048518399141faad
Aditya pratap singh <72407189+roxy-jr@users.noreply.github.com>
2020-11-04 23:26:38 +0530

    Add files via upload

---
+++ duty.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class duty
+{
+public static void main(String []gr)
+{
+char i;
+System.out.println("enter the number of row");
+Scanner s1=new Scanner(System.in);
+int n=s1.nextInt();
+for(i=97;i<=122;i++)
+{
+System.out.println(i);
+}
+}
+}
+
+
