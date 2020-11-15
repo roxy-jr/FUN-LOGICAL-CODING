d31297c8d45c7ad1f20260abe363cd1bb2f46b6b
Aditya pratap singh <72407189+roxy-jr@users.noreply.github.com>
2020-11-15 21:16:41 +0530

    Add files via upload

---
+++ d.java
@@ -0,0 +1,19 @@
+public class D
+{
+private int x;
+public D(int x)
+{
+this.x=x;
+}
+public void show()
+{ 
+int x=21;
+System.out.println(x);
+}
+public static void main(String []args)
+{
+D ob=new D(10);
+ob.show();
+}
+}
+
