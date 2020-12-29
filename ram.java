0e7e2b69ab11ae93a73aadfaa68639273f464c9c
Aditya pratap singh <72407189+roxy-jr@users.noreply.github.com>
2020-12-29 09:26:35 -0800

    Add files via upload

---
+++ ram.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class ram
+{
+public static void main(String[]ar)
+{
+System.out.println("enter the value of column");
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int r=n;r>=1;r--)
+{
+for(int c=1;c<=r;c++)
+{
+System.out.print("*");
+System.out.print("\n");
+//for(int c=4;c<=1 ;c--)
+//{
+	//System.out.println(" ");
+//}
+}
+}
+}
+}
